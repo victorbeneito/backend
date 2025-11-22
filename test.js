dda0414e557cc46f18840e4f77cd4f017761b8f1
Victor Beneyto <liberiogasss@gmail.com>
2025-11-22 11:21:12 +0100

    Eliminem Data i test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/test', (req, res) => res.send('Test OK'));
-
-const listEndpoints = require('express-list-endpoints');
-console.log(listEndpoints(app)); // Esperamos '/test' aquí
-
-app.listen(3000);
